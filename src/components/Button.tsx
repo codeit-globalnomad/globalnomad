b6388365df0d32dda45308330877cc18f6efc1a6
qpalkim <ghs0199@naver.com>
2025-03-17 15:13:20 +0900

    refactor: 버튼 컴포넌트 개선

---
+++ src/components/Button.tsx
@@ -0,0 +1,38 @@
+'use client';
+import { cva, type VariantProps } from 'class-variance-authority';
+import { cn } from '@/lib/cn';
+
+const buttonVariants = cva('cursor-pointer transition-all duration-200 flex items-center justify-center', {
+  variants: {
+    variant: {
+      default: 'rounded-md bg-green-100 font-bold text-lg text-white hover:bg-black-200',
+      outline:
+        'rounded-md border border-black-200 font-bold text-lg text-black-200 hover:bg-black-200 hover:text-white',
+    },
+    disabled: {
+      true: 'bg-gray-600 text-white cursor-default hover:bg-gray-600 hover:text-white',
+      false: '',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+    disabled: false,
+  },
+});
+
+interface ButtonProps extends VariantProps<typeof buttonVariants> {
+  text: string;
+  onClick?: () => void;
+  className?: string;
+  disabled?: boolean;
+}
+
+export default function Button({ text, onClick, disabled = false, className, variant }: ButtonProps) {
+  const finalClassName = cn(buttonVariants({ variant, disabled }), className || '');
+
+  return (
+    <button onClick={!disabled ? onClick : undefined} disabled={disabled} className={finalClassName}>
+      {text}
+    </button>
+  );
+}
