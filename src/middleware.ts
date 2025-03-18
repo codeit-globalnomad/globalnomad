eb57e3d53d354924bfe5a773ef07a5f4c0124a58
nagyum <wildanimal19@naver.com>
2025-03-18 14:24:37 +0900

    fix:로그인후 랜딩페이지 이동 가능

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ const AFTER_LOGIN_DOMAIN = [
   '/my-reservations',
 ] satisfies readonly string[];
 
-const BEFORE_LOGIN_DOMAIN = ['/', '/login', '/signup'] satisfies readonly string[];
+const BEFORE_LOGIN_DOMAIN = ['/login', '/signup'] satisfies readonly string[];
 
 export const middleware = async (request: NextRequest) => {
   const cookieStore = await cookies();
@@ -16,17 +16,27 @@ export const middleware = async (request: NextRequest) => {
   const refreshToken = cookieStore.get('refreshToken')?.value;
 
   const pathname = request.nextUrl.pathname;
-
-  const isAfterLoginRoute = AFTER_LOGIN_DOMAIN.some((route) => pathname.startsWith(route));
-  const isBeforeLoginRoute = BEFORE_LOGIN_DOMAIN.includes(pathname);
   const isLoggedIn = accessToken || refreshToken;
 
-  if (isBeforeLoginRoute) {
+  const isAfterLoginRoute = pathname !== '/' && AFTER_LOGIN_DOMAIN.some((route) => pathname.startsWith(route));
+  const isBeforeLoginRoute = BEFORE_LOGIN_DOMAIN.includes(pathname);
+
+  if (pathname === '/') {
     if (isLoggedIn) {
-      return NextResponse.redirect(new URL('/activities', request.nextUrl));
+      return NextResponse.next();
     }
     return NextResponse.next();
   }
+
+  if (isBeforeLoginRoute) {
+    if (isLoggedIn) {
+      if (pathname === '/login' || pathname === '/signup') {
+        return NextResponse.redirect(new URL('/activities', request.nextUrl));
+      }
+    }
+    return NextResponse.next();
+  }
+
   if (isAfterLoginRoute) {
     if (!isLoggedIn) {
       return NextResponse.redirect(new URL('/login', request.nextUrl));
