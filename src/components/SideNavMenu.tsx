0b16eeb63eba4558ba2682572251586892672444
Charie95 <lus0905@gmail.com>
2025-03-22 12:34:55 +0900

    주석제거

--- src/components/SideNavMenu.tsx
+++ src/components/SideNavMenu.tsx
@@ -5,12 +5,10 @@ import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
 import ProfileImage from '@/components/ProfileImage';
-
-// SVG 아이콘
-import MyInfo from '@/assets/icons/my-users.svg'; // 내정보
-import MyReservation from '@/assets/icons/my-reservations.svg'; // 예약내역
-import MyActivities from '@/assets/icons/my-activities.svg'; // 내 체험 관리
-import MyReservationStatus from '@/assets/icons/my-activities-dashboard.svg'; // 예약 현황
+import MyInfo from '@/assets/icons/my-users.svg';
+import MyReservation from '@/assets/icons/my-reservations.svg';
+import MyActivities from '@/assets/icons/my-activities.svg';
+import MyReservationStatus from '@/assets/icons/my-activities-dashboard.svg';
 import EditIcon from '@/assets/icons/pencil.svg';
 
 const NAV_ITEMS = [
@@ -24,7 +22,6 @@ const SideNavMenu = () => {
   const pathname = usePathname();
   const [previewImage, setPreviewImage] = useState<string | null>(null);
 
-  // 파일 선택 핸들러
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file) {
@@ -35,11 +32,8 @@ const SideNavMenu = () => {
 
   return (
     <aside className='w-[251px] flex-none rounded-lg border border-gray-300 bg-white p-4 shadow-md md:w-[384px]'>
-      {/* 프로필 이미지 */}
       <div className='relative flex flex-col items-center gap-4'>
         <ProfileImage size='large' src={previewImage || undefined} />
-
-        {/* 프로필 수정 버튼 */}
         <label
           className='hover:bg-green-10 absolute right-7 bottom-0 flex h-11 w-11 cursor-pointer items-center justify-center rounded-full bg-green-100 transition md:right-24'
           aria-label='프로필 수정'
@@ -48,8 +42,6 @@ const SideNavMenu = () => {
           <input type='file' accept='image/*' className='hidden' onChange={handleFileChange} />
         </label>
       </div>
-
-      {/* 네비게이션 메뉴 */}
       <nav className='mt-6'>
         <ul className='flex flex-col gap-1'>
           {NAV_ITEMS.map((item) => {
