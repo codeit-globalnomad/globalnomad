05ed97f2df3e2f36d286d88e2ce4c5fcb3dd31df
qpalkim <ghs0199@naver.com>
2025-03-22 18:56:32 +0900

    pref: useClickOutside 함수 모바일 환경 적용

--- src/lib/utils/useClickOutside.tsx
+++ src/lib/utils/useClickOutside.tsx
@@ -2,16 +2,18 @@ import { useEffect } from 'react';
 
 export function useClickOutside(ref: React.RefObject<HTMLElement | null>, handler: () => void) {
   useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
+    const handleClickOutside = (event: MouseEvent | TouchEvent) => {
       if (ref.current && !ref.current.contains(event.target as Node)) {
         handler();
       }
     };
 
     document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('touchstart', handleClickOutside);
 
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('touchstart', handleClickOutside);
     };
   }, [ref, handler]);
 }
