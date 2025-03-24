45b050587f12afd9c562aa5db33866257c59a615
Hyesun Kim <cocoabearhot@naver.com>
2025-03-24 12:58:51 +0900

    fix: 로딩 스피너 위치 조정

--- src/app/(with-nav-footer)/activities/[id]/_components/LoadingSpinner.tsx
+++ src/app/(with-nav-footer)/activities/[id]/_components/LoadingSpinner.tsx
@@ -6,7 +6,7 @@ const LoadingSpinner = () => (
       position: 'relative',
     }}
   >
-    <div className='absolute inset-0 top-[-120px] flex items-center justify-center'>
+    <div className='absolute inset-0 top-[-70px] flex items-center justify-center'>
       <div className='spinner-border h-16 w-16 animate-spin rounded-full border-4 border-solid border-green-100 border-t-transparent'></div>
     </div>
   </div>
