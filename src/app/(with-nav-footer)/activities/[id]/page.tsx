2785ab2696ee8bb61c845a21ede0291b5fd4b4f6
Hyesun Kim <cocoabearhot@naver.com>
2025-03-24 20:47:46 +0900

    fix: 빌드 오류 해결, 파일명 변경

--- src/app/(with-nav-footer)/activities/[id]/page.tsx
+++ src/app/(with-nav-footer)/activities/[id]/page.tsx
@@ -2,7 +2,7 @@ import { notFound } from 'next/navigation';
 import axiosServerHelper from '@/lib/network/axiosServerHelper';
 import { safeResponse } from '@/lib/network/safeResponse';
 import { Activity, activityDetailSchema } from '@/lib/types/activities';
-import ActivityDetailPage from './ActivityDetail/page';
+import ActivityDetailPage from './ActivityDetail';
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const id = (await params).id;
