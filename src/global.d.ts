895b66669c3247fbd4df548cdb58eab5f6031153
Hyesun Kim <cocoabearhot@naver.com>
2025-03-25 16:47:18 +0900

    feat: global.d.ts에 페이스북 타입 추가

--- src/global.d.ts
+++ src/global.d.ts
@@ -27,6 +27,37 @@ declare global {
         sendDefault: (options: KakaoShareButtonOptions) => void;
       };
     };
+    FB: {
+      getLoginStatus: (callback: (response: FB.LoginStatusResponse) => void) => void;
+      login: (callback: (response: FB.LoginResponse) => void) => void;
+      ui: (params: FB.ShareParams, callback: (response: FB.ShareResponse) => void) => void;
+    };
+  }
+}
+
+declare namespace FB {
+  export interface LoginStatusResponse {
+    status: 'connected' | 'not_authorized' | 'unknown';
+    authResponse?: LoginResponse;
+  }
+
+  export interface LoginResponse {
+    authResponse: {
+      accessToken: string;
+      expiresIn: number;
+      signedRequest: string;
+      userID: string;
+    };
+  }
+
+  export interface ShareResponse {
+    error_message?: string;
+    post_id?: string;
+  }
+
+  export interface ShareParams {
+    method: 'share';
+    href: string;
   }
 }
 
