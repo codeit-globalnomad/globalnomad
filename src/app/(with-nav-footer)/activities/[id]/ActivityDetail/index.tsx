a811f54828297359c206a3d080e359836f49b5ea
Hyesun Kim <cocoabearhot@naver.com>
2025-03-25 20:50:38 +0900

    fix: 더미 데이터 삭제 및 불필요한 변수 제거

--- src/app/(with-nav-footer)/activities/[id]/ActivityDetail/index.tsx
+++ src/app/(with-nav-footer)/activities/[id]/ActivityDetail/index.tsx
@@ -1,6 +1,9 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import { useActivityDetail } from '@/lib/hooks/useActivities';
+import { useMyData } from '@/lib/hooks/useUsers';
+import { useIntersectionObserver } from '@/lib/utils/useIntersectionObserver';
 import ActivityHeader from './_components/ActivityHeader';
 import ActivityGallery from './_components/ActivityGallery';
 import ActivityTab from './_components/ActivityTab';
@@ -11,8 +14,6 @@ import { MobileReservation, TabletReservation, DesktopReservation } from './_com
 import ActivityBanner from './_components/ActivityBanner';
 import ScrollToTopButton from './_components/ScrollToTopButton';
 import LoadingSpinner from './_components/LoadingSpinner';
-import { useActivityDetail } from '@/lib/hooks/useActivities';
-import { useIntersectionObserver } from '@/lib/utils/useIntersectionObserver';
 
 const wrapper = 'mt-3 flex w-full flex-col gap-4 md:gap-6 max-w-[1200px]';
 const tabItems = [
@@ -23,14 +24,10 @@ const tabItems = [
 
 export default function ActivityDetailPage({ id }: { id: number }) {
   const { data: activityDetail } = useActivityDetail(id);
+  const { data: userData } = useMyData();
 
   const [currentTab, setCurrentTab] = useState('description');
   const [isLoading, setIsLoading] = useState(true);
-  const [hasReservationSection, setHasReservationSection] = useState(false);
-
-  // 임의의 더미 데이터
-  const currentUserId = 1; // 현재 로그인된 유저 ID
-  const activityCreatorId = 2; // 활동 생성자 ID
 
   useIntersectionObserver((id) => setCurrentTab(id));
 
@@ -42,10 +39,6 @@ export default function ActivityDetailPage({ id }: { id: number }) {
   };
 
   useEffect(() => {
-    if (Number(currentUserId) !== Number(activityCreatorId)) {
-      setHasReservationSection(true);
-    }
-
     scrollToTop();
     const timer = setTimeout(() => {
       setIsLoading(false);
@@ -54,22 +47,24 @@ export default function ActivityDetailPage({ id }: { id: number }) {
     return () => clearTimeout(timer);
   }, []);
 
-  if (isLoading || !activityDetail) {
+  if (isLoading || !activityDetail || !userData) {
     return <LoadingSpinner />;
   }
+
+  const isSameUser = userData.id === activityDetail.userId;
 
   return (
     <div className='relative flex flex-col items-center justify-center scroll-smooth'>
       <div className={`${wrapper} flex-col px-5 lg:mt-10`}>
         <section className='flex flex-col gap-4'>
-          <ActivityHeader activityDetail={activityDetail} />
+          <ActivityHeader activityDetail={activityDetail} isSameUser={isSameUser} />
         </section>
       </div>
       <div className={`${wrapper} flex-col md:px-5`}>
         <ActivityGallery />
       </div>
       <div className={`md:${wrapper} px-5 md:flex-row md:gap-[2%] lg:mb-16`}>
-        <section className={`mt-6 mb-6 w-full ${hasReservationSection ? 'md:w-[70%]' : 'md:w-full'}`}>
+        <section className={`mt-6 mb-6 w-full ${!isSameUser ? 'md:w-[70%]' : 'md:w-full'}`}>
           <div className='sticky top-0 z-10 bg-white'>
             <ActivityTab
               tabs={tabItems}
@@ -86,7 +81,7 @@ export default function ActivityDetailPage({ id }: { id: number }) {
             <ActivityBanner />
           </div>
         </section>
-        {hasReservationSection && (
+        {!isSameUser && (
           <section className='md: fixed bottom-0 left-0 z-50 w-full md:relative md:top-0 md:right-0 md:w-[28%]'>
             <MobileReservation />
             <div className='sticky top-3 md:mt-6 md:mb-3'>
