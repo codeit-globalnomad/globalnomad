20e3ba87c72eb9f7f170b58c1acfabcdff20ef43
qpalkim <ghs0199@naver.com>
2025-03-29 11:13:26 +0900

    build: Suspense boundary 추가 및 빌드 오류

--- src/app/(with-nav-footer)/activities/page.tsx
+++ src/app/(with-nav-footer)/activities/page.tsx
@@ -1,11 +1,15 @@
 import Banner from './_components/Banner';
 import ActivityList from './_components/ActivityList';
+import { Suspense } from 'react';
+import LoadingSpinner from '@/components/LoadingSpinner';
 
 export default function Page() {
   return (
     <>
       <Banner />
-      <ActivityList />
+      <Suspense fallback={<LoadingSpinner />}>
+        <ActivityList />
+      </Suspense>
     </>
   );
 }
