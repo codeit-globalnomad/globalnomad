3cacc2672238ca95102a82e25bf6c027b14d85cb
kimsoyoung <sy_busay@naver.com>
2025-03-30 03:34:31 +0900

    style: 팝업창 스타일 변경

--- src/app/(with-nav-footer)/(mypage)/my-activities/[id]/reservation-dashboard/_components/MyActivitiesReservations.tsx
+++ src/app/(with-nav-footer)/(mypage)/my-activities/[id]/reservation-dashboard/_components/MyActivitiesReservations.tsx
@@ -86,7 +86,7 @@ export default function MyActivitiesReservations({ selectedDate, setSelectedDate
   return (
     <div
       ref={dateRef}
-      className='text-black-100 absolute top-[220px] right-0 flex h-[697px] min-h-[582px] w-[429px] flex-col gap-[27px] overflow-hidden rounded-[24px] border border-gray-300 bg-white px-6 py-6'
+      className='text-black-100 absolute top-[220px] right-0 flex h-[697px] min-h-[582px] w-[429px] flex-col gap-[27px] overflow-hidden rounded-[24px] border border-gray-300 bg-white px-6 py-6 shadow-md'
     >
       <div className='flex items-center justify-between'>
         <h1 className='text-[24px] leading-[32px] font-bold'>예약 정보</h1>
@@ -116,35 +116,47 @@ export default function MyActivitiesReservations({ selectedDate, setSelectedDate
       </div>
       {filteredSchedule && filteredSchedule.length > 0 ? (
         <>
-          <section className='flex items-center gap-5 border-b border-b-gray-300 text-[20px] leading-[32px] font-normal text-gray-900'>
-            <button
-              className={`cursor-pointer ${activeTab === 'pending' ? 'pm-[6px] border-b-[4px] border-green-100 text-[20px] leading-[32px] font-semibold text-green-100' : ''}`}
-              onClick={() => setActiveTab('pending')}
-            >
-              신청 {filteredSchedule[0].count.pending}
-            </button>
-            <button
-              className={`cursor-pointer ${activeTab === 'confirmed' ? 'pm-[6px] border-b-[4px] border-green-100 text-[20px] leading-[32px] font-semibold text-green-100' : ''}`}
-              onClick={() => setActiveTab('confirmed')}
-            >
-              승인 {filteredSchedule[0].count.confirmed}
-            </button>
-            <button
-              className={`cursor-pointer ${activeTab === 'declined' ? 'pm-[6px] border-b-[4px] border-green-100 text-[20px] leading-[32px] font-semibold text-green-100' : ''}`}
-              onClick={() => setActiveTab('declined')}
-            >
-              거절 {filteredSchedule[0].count.declined}
-            </button>
-          </section>
-          {activeTab && selectedSchedule && (
-            <ReservationCardList type={activeTab} scheduleId={Number(selectedSchedule.value)} activityId={activityId} />
-          )}
+          <div className='h-[500px] rounded-b-[24px] bg-gray-100'>
+            <section className='flex items-center gap-5 border-b border-b-gray-300 bg-white text-[20px] leading-[32px] font-normal text-gray-900'>
+              <button
+                className={`cursor-pointer ${activeTab === 'pending' ? 'pm-[6px] border-b-[4px] border-green-100 text-[20px] leading-[32px] font-semibold text-green-100' : ''}`}
+                onClick={() => setActiveTab('pending')}
+              >
+                신청 {filteredSchedule[0].count.pending}
+              </button>
+              <button
+                className={`cursor-pointer ${activeTab === 'confirmed' ? 'pm-[6px] border-b-[4px] border-green-100 text-[20px] leading-[32px] font-semibold text-green-100' : ''}`}
+                onClick={() => setActiveTab('confirmed')}
+              >
+                승인 {filteredSchedule[0].count.confirmed}
+              </button>
+              <button
+                className={`cursor-pointer ${activeTab === 'declined' ? 'pm-[6px] border-b-[4px] border-green-100 text-[20px] leading-[32px] font-semibold text-green-100' : ''}`}
+                onClick={() => setActiveTab('declined')}
+              >
+                거절 {filteredSchedule[0].count.declined}
+              </button>
+            </section>
+
+            {activeTab && selectedSchedule && (
+              <div className='px-2 pt-[27px]'>
+                <ReservationCardList
+                  type={activeTab}
+                  scheduleId={Number(selectedSchedule.value)}
+                  activityId={activityId}
+                />
+              </div>
+            )}
+          </div>
         </>
       ) : (
-        <div className='flex flex-col items-center gap-4 px-[50px] py-[145px]'>
-          <Image src={NoData} width={80} height={80} alt='선택된 시간대에 대한 예약 정보가 없습니다.' />
-          <div className='items-center justify-center text-center text-gray-500'>
-            선택된 시간대에 대한 예약 정보가 없습니다.
+        <div className='rounded-b-[24px] bg-gray-100'>
+          <div className='h-[1px] w-full bg-gray-300'></div>
+          <div className='flex flex-col items-center gap-4 px-[50px] py-[145px]'>
+            <Image src={NoData} width={80} height={80} alt='선택된 시간대에 대한 예약 정보가 없습니다.' />
+            <div className='items-center justify-center text-center text-gray-500'>
+              선택된 시간대에 대한 예약 정보가 없습니다.
+            </div>
           </div>
         </div>
       )}
--- src/app/(with-nav-footer)/(mypage)/my-activities/[id]/reservation-dashboard/_components/ReservationDetails.tsx
+++ src/app/(with-nav-footer)/(mypage)/my-activities/[id]/reservation-dashboard/_components/ReservationDetails.tsx
@@ -7,10 +7,10 @@ type Props = {
   nickname: string;
   headCount: number;
 };
-// 카드리스트에서 데이터 전달할 에정
+
 export default function ReservationDetails({ type, nickname, headCount }: Props) {
   return (
-    <div className='w-full rounded-[4px] border border-gray-300 px-4 pt-[9px] pb-3'>
+    <div className='w-full rounded-[4px] border border-gray-300 bg-white px-4 pt-[9px] pb-3 shadow-sm'>
       <div className='flex gap-[10px] text-[16px] leading-[26px]'>
         <div className='font-semibold text-gray-800'>닉네임</div>
         <span className='text-black-100 font-medium'>{nickname}</span>
