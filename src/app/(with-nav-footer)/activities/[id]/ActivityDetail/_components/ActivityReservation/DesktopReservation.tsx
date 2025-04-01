d2c990466b9105cbeba847542301caa159e75856
Hyesun Kim <cocoabearhot@naver.com>
2025-04-01 23:37:20 +0900

    refactor: 컴포넌트 분리 및 타입 수정

--- src/app/(with-nav-footer)/activities/[id]/ActivityDetail/_components/ActivityReservation/DesktopReservation.tsx
+++ src/app/(with-nav-footer)/activities/[id]/ActivityDetail/_components/ActivityReservation/DesktopReservation.tsx
@@ -1,23 +1,21 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
-import { format, isBefore, startOfDay } from 'date-fns';
-import Calendar from 'react-calendar';
+import { format } from 'date-fns';
 import { useForm, Controller } from 'react-hook-form';
 import { toast } from 'react-toastify';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Keyboard } from 'swiper/modules';
-import { Swiper as SwiperType } from 'swiper/types';
 import { useQueryClient } from '@tanstack/react-query';
 import alert from '@/assets/icons/alert.svg';
 import decrement from '@/assets/icons/decrement.svg';
 import increment from '@/assets/icons/increment.svg';
-import leftArrow from '@/assets/icons/left-arrow.svg';
 import { useAvailableSchedule, useCreateReservation } from '@/lib/hooks/useActivities';
 import { useMyReservations } from '@/lib/hooks/useMyReservation';
 import 'react-calendar/dist/Calendar.css';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import './CalendarStyles.css';
+import DesktopCalender from './DesktopCalendar';
+import DesktopAvailableTime from './DesktopAvailableTime';
+import Button from '@/components/Button';
 
 type DesktopReservationProps = {
   isLoggedIn: boolean;
@@ -42,7 +40,7 @@ export default function DesktopReservation({ isLoggedIn, currentActivityId, pric
   const selectedYear = String(today.getFullYear());
   const selectedMonth = String(today.getMonth() + 1).padStart(2, '0');
 
-  const [selectedDate, setSelectedDate] = useState<Date | undefined>(new Date());
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>(new Date() || today);
   const [availableTimes, setAvailableTimes] = useState<{ id: number; startTime: string; endTime: string }[]>([]);
   const [selectedTimeId, setSelectedTimeId] = useState<number | null>(null);
   const [size, setSize] = useState<number>(10);
@@ -54,7 +52,6 @@ export default function DesktopReservation({ isLoggedIn, currentActivityId, pric
   const totalPrice = price * peopleCount;
 
   const queryClient = useQueryClient();
-  const swiperRef = useRef<SwiperType | null>(null);
 
   const { data: availableSchedule } = useAvailableSchedule(currentActivityId, selectedYear, selectedMonth);
   const { data: myReservations } = useMyReservations({
@@ -64,17 +61,8 @@ export default function DesktopReservation({ isLoggedIn, currentActivityId, pric
 
   const availableDates = availableSchedule?.map((schedule) => new Date(schedule.date)) || [];
 
-  const isDateDisabled = (date: Date) => {
-    return isBefore(startOfDay(date), startOfDay(today));
-  };
-
   const handleTimeSelection = (timeId: number) => {
     setSelectedTimeId((prevTimeId) => (prevTimeId === timeId ? null : timeId));
-  };
-
-  const formatCalendarDay = (locale: string | undefined, date: Date): string => {
-    const day = date.getDate();
-    return day < 10 ? `0${day}` : `${day}`;
   };
 
   useEffect(() => {
@@ -99,15 +87,9 @@ export default function DesktopReservation({ isLoggedIn, currentActivityId, pric
       const selectedDateStr = format(selectedDate, 'yyyy-MM-dd');
       const schedule = availableSchedule?.find(({ date }) => date === selectedDateStr);
       setAvailableTimes(schedule?.times || []);
-      swiperRef.current?.slideTo(0);
+      setSelectedTimeId(null);
     }
   }, [selectedDate, availableSchedule]);
-
-  useEffect(() => {
-    if (swiperRef.current) {
-      swiperRef.current.update();
-    }
-  }, [disabledTimeIds]);
 
   useEffect(() => {
     if (selectedDate || selectedTimeId) {
@@ -133,8 +115,6 @@ export default function DesktopReservation({ isLoggedIn, currentActivityId, pric
           setValue('people', 1);
 
           setDisabledTimeIds((prev) => [...prev, Number(selectedTimeId)]);
-          console.log('Updated disabledTimeIds:', [...disabledTimeIds, Number(selectedTimeId)]);
-
           queryClient.invalidateQueries({
             queryKey: ['availableSchedule', currentActivityId, selectedYear, selectedMonth],
           });
@@ -162,125 +142,26 @@ export default function DesktopReservation({ isLoggedIn, currentActivityId, pric
               control={control}
               rules={{ required: '날짜를 선택해주세요.' }}
               render={({ field }) => (
-                <div>
-                  <Calendar
-                    className='desktopReservation'
-                    value={selectedDate || today}
-                    onClickDay={(date) => {
-                      if (!isDateDisabled(date)) {
-                        setSelectedDate(date);
-                        field.onChange(format(date, 'yyyy-MM-dd'));
-                      }
-                    }}
-                    minDate={today}
-                    tileClassName={({ date }) => {
-                      let className = '';
-                      if (
-                        availableDates.some(
-                          (availableDate) => format(availableDate, 'yyyy-MM-dd') === format(date, 'yyyy-MM-dd'),
-                        )
-                      ) {
-                        className = 'react-calendar__tile--available';
-                      }
-                      if (selectedDate && format(selectedDate, 'yyyy-MM-dd') === format(date, 'yyyy-MM-dd')) {
-                        className += ' react-calendar__tile--selected';
-                      }
-                      if (format(today, 'yyyy-MM-dd') === format(date, 'yyyy-MM-dd')) {
-                        if (selectedDate) {
-                          className += ' react-calendar__tile--today-underline';
-                        } else {
-                          className += ' react-calendar__tile--now';
-                        }
-                      }
-
-                      return className;
-                    }}
-                    locale='ko'
-                    calendarType='gregory'
-                    prev2Label={null}
-                    next2Label={null}
-                    formatDay={formatCalendarDay}
-                  />
-                </div>
+                <DesktopCalender
+                  control={control}
+                  today={today}
+                  availableDates={availableDates}
+                  selectedDate={selectedDate}
+                  setSelectedDate={setSelectedDate}
+                  field={field}
+                />
               )}
             />
           </li>
           <li className='flex flex-col gap-2'>
-            <div className='flex items-center justify-between'>
-              <p className='text-xl font-bold'>예약 가능한 시간</p>
-              <div className='z-10 mr-[-10px] flex cursor-pointer'>
-                <div className={`custom-prev ${currentIndex === 0 ? 'pointer-events-none opacity-50' : ''}`}>
-                  <Image src={leftArrow} width={26} height={26} alt='이전 화살표 아이콘' />
-                </div>
-                <div
-                  className={`custom-next ${currentIndex === availableTimes.length - 1 ? 'pointer-events-none opacity-50' : ''}`}
-                >
-                  <Image
-                    src={leftArrow}
-                    width={26}
-                    height={26}
-                    alt='다음 화살표 아이콘'
-                    className='scale-x-[-1] transform'
-                  />
-                </div>
-              </div>
-            </div>
-            <div className='flex space-x-2'>
-              {availableTimes.length > 0 ? (
-                <Swiper
-                  modules={[Navigation, Keyboard]}
-                  onSwiper={(swiper) => {
-                    swiperRef.current = swiper;
-                  }}
-                  onSlideChange={(swiper) => {
-                    setCurrentIndex(swiper.activeIndex);
-                  }}
-                  navigation={{
-                    nextEl: '.custom-next',
-                    prevEl: '.custom-prev',
-                  }}
-                  keyboard={{
-                    enabled: true,
-                  }}
-                  spaceBetween={10}
-                  slidesPerView='auto'
-                  loop={false}
-                  className='m-0 h-auto'
-                  centeredSlides={false}
-                  key={disabledTimeIds.length}
-                >
-                  {availableTimes.map((timeSlot) => (
-                    <SwiperSlide
-                      key={`${timeSlot.id}-${selectedTimeId}-${selectedDate}`}
-                      style={{
-                        width: 'auto',
-                        flexShrink: 0,
-                        display: 'flex',
-                        alignItems: 'center',
-                        pointerEvents: disabledTimeIds.includes(timeSlot.id) ? 'none' : 'auto',
-                      }}
-                    >
-                      <label
-                        className={`cursor-pointer rounded-md border-[1px] p-2 text-lg whitespace-nowrap ${
-                          selectedTimeId === timeSlot.id ? 'bg-green-100 text-white' : ''
-                        } ${disabledTimeIds.includes(timeSlot.id) ? 'cursor-not-allowed border-gray-400 bg-gray-200 text-gray-500' : 'border-green-100'}`}
-                      >
-                        <input
-                          type='radio'
-                          value={`${timeSlot.startTime} - ${timeSlot.endTime}`}
-                          onChange={() => handleTimeSelection(timeSlot.id)}
-                          className='hidden'
-                          disabled={disabledTimeIds.includes(timeSlot.id)}
-                        />
-                        {timeSlot.startTime} - {timeSlot.endTime}
-                      </label>
-                    </SwiperSlide>
-                  ))}
-                </Swiper>
-              ) : (
-                <></>
-              )}
-            </div>
+            <DesktopAvailableTime
+              availableTimes={availableTimes}
+              disabledTimeIds={disabledTimeIds}
+              selectedTimeId={selectedTimeId}
+              onTimeSelection={handleTimeSelection}
+              currentIndex={currentIndex}
+              onIndexChange={setCurrentIndex}
+            />
             <p className='flex gap-1 text-[15px] text-[#767676]'>
               <Image src={alert} width={16} height={16} alt='경고 아이콘' />
               {availableTimes.length === 0
@@ -294,7 +175,7 @@ export default function DesktopReservation({ isLoggedIn, currentActivityId, pric
             <div className='flex w-[120px] items-center rounded-md border border-gray-300'>
               <button
                 type='button'
-                className='w-[40px] p-2 text-center'
+                className='w-[40px] cursor-pointer p-2 text-center'
                 onClick={() => setValue('people', peopleCount - 1)}
                 aria-label='증가 버튼'
               >
@@ -304,11 +185,11 @@ export default function DesktopReservation({ isLoggedIn, currentActivityId, pric
                 type='text'
                 {...register('people', { min: 1, required: true })}
                 readOnly
-                className='w-[40px] p-2 text-center'
+                className='w-[40px] cursor-pointer p-2 text-center'
               />
               <button
                 type='button'
-                className='w-[40px] p-2 text-center'
+                className='w-[40px] cursor-pointer p-2 text-center'
                 onClick={() => setValue('people', peopleCount + 1)}
                 aria-label='감소 버튼'
               >
@@ -326,7 +207,7 @@ export default function DesktopReservation({ isLoggedIn, currentActivityId, pric
             <span className='text-[30px]'>₩ {totalPrice.toLocaleString()}</span>
           </li>
         </ol>
-        <button
+        <Button
           type='submit'
           className={`h-[60px] w-full cursor-pointer rounded-t-none rounded-b-md py-2 font-bold text-white ${
             !selectedTimeId || !isLoggedIn ? 'cursor-not-allowed bg-gray-300' : 'bg-green-100 hover:bg-green-100'
@@ -334,7 +215,7 @@ export default function DesktopReservation({ isLoggedIn, currentActivityId, pric
           disabled={!selectedTimeId || !isLoggedIn || reservationCompleted}
         >
           {reservationCompleted ? '예약 완료' : '예약하기'}
-        </button>
+        </Button>
       </form>
     </div>
   );
