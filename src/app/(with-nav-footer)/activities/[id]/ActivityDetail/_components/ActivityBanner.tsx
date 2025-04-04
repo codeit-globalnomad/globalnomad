8a6cdd574333983db662e3a133188bd00b6f2ec9
Hyesun Kim <cocoabearhot@naver.com>
2025-04-04 22:41:48 +0900

    refactor: activity 객체에 spread 연산자 사용해 banners 매핑 리팩토링

--- src/app/(with-nav-footer)/activities/[id]/ActivityDetail/_components/ActivityBanner.tsx
+++ src/app/(with-nav-footer)/activities/[id]/ActivityDetail/_components/ActivityBanner.tsx
@@ -1,15 +1,17 @@
+'use client';
+
 import { useEffect, useRef, useState } from 'react';
-import Link from 'next/link';
 import Image from 'next/image';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import Link from 'next/link';
 import { Swiper as SwiperInstance } from 'swiper';
 import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import leftArrow from '@/assets/icons/left-arrow.svg';
 import starRating from '@/assets/icons/star-rating.svg';
 import { useActivities } from '@/lib/hooks/useActivities';
 import { GetActivitiesParams } from '@/lib/types/activities';
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 interface ActivityBannerProps {
   category: string;
@@ -29,6 +31,7 @@ interface Banner {
 export default function ActivityBanner({ category, currentActivityId }: ActivityBannerProps) {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [selectedBanners, setSelectedBanners] = useState<Banner[]>([]);
+
   const swiperRef = useRef<SwiperInstance | null>(null);
 
   const params: GetActivitiesParams = {
@@ -41,13 +44,9 @@ export default function ActivityBanner({ category, currentActivityId }: Activity
   useEffect(() => {
     if (data?.activities) {
       const banners = data.activities.map((activity) => ({
-        id: activity.id,
+        ...activity,
         imageUrl: activity.bannerImageUrl,
         alt: activity.title || '추천 체험 이미지',
-        title: activity.title,
-        rating: activity.rating,
-        price: activity.price,
-        reviewCount: activity.reviewCount,
       }));
 
       const filteredBanners = banners.filter((banner) => banner.id !== currentActivityId);
@@ -68,7 +67,7 @@ export default function ActivityBanner({ category, currentActivityId }: Activity
     <>
       <div className='pt-[40px] md:pt-[50px]'></div>
       <div className='relative flex items-center justify-between'>
-        <h3 className='text-xl font-bold'>{category} 추천 체험</h3>
+        <h3 className='text-xl font-bold md:text-[22px]'>{category} 추천 체험</h3>
         <div className='z-10 mr-[-10px] flex cursor-pointer'>
           <div className={`custom-prev-banner ${currentIndex === 0 ? 'pointer-events-none opacity-50' : ''}`}>
             <Image src={leftArrow} width={32} height={32} alt='이전 화살표 아이콘' />
@@ -106,11 +105,11 @@ export default function ActivityBanner({ category, currentActivityId }: Activity
                 <div className='relative h-full w-full'>
                   <Image
                     src={banner.imageUrl}
-                    alt={banner.alt}
                     fill
                     sizes='(max-width: 768px) 100vw, 768px'
                     className='object-cover object-center'
                     priority={false}
+                    alt={banner.alt}
                   />
                 </div>
                 <div className='absolute top-0 right-0 bottom-0 left-0 bg-gradient-to-t from-black to-transparent opacity-60'></div>
@@ -120,7 +119,7 @@ export default function ActivityBanner({ category, currentActivityId }: Activity
                       <div className='flex items-center gap-1'>
                         <Image src={starRating} width={14} height={14} alt='별점 아이콘' />
                         <span className='text-md'>
-                          {banner.rating}({banner.reviewCount})
+                          {banner.rating} ({banner.reviewCount})
                         </span>
                       </div>
                       <h4 className='w-full overflow-hidden text-2xl font-bold text-ellipsis whitespace-nowrap text-white md:text-2xl lg:text-2xl'>
