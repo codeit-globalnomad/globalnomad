5fac30d239b3c0d66719d623d29c39315dc0ec81
Charie95 <lus0905@gmail.com>
2025-04-05 12:16:18 +0900

    refactor: img 경고 메시지 에러 해결

--- src/components/SideNavMenu.tsx
+++ src/components/SideNavMenu.tsx
@@ -67,8 +67,8 @@ const SideNavMenu = ({ activityId }: SideNavMenuProps) => {
             className='hover:bg-green-10 absolute -right-0 -bottom-0 flex h-11 w-11 cursor-pointer items-center justify-center rounded-full bg-green-100 transition'
             aria-label='프로필 수정'
           >
-            <Image src={EditIcon} alt='프로필 수정' width={24} height={24} />
-            <input type='file' accept='image/*' className='hidden' onChange={handleFileChange} />
+            <Image src={EditIcon} alt='프로필 수정' />
+            <input type='file' accept='image/*' className='hidden h-[24px] w-[24px]' onChange={handleFileChange} />
           </label>
         </div>
       </div>
