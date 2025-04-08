4be6fe0d4bf18e79bcb7dc82a93ad12bfcb2a0c2
kimsoyoung <sy_busay@naver.com>
2025-04-08 17:38:02 +0900

    fix: reservation-dashboard 페이지에 empty 컴포넌트 연결

--- src/app/(with-nav-footer)/(mypage)/my-activities/reservation-dashboard/page.tsx
+++ src/app/(with-nav-footer)/(mypage)/my-activities/reservation-dashboard/page.tsx
@@ -1,16 +1,10 @@
-import Empty from '@/assets/icons/empty.svg';
-import Image from 'next/image';
+import Empty from '@/components/Empty';
 
 export default function NotActivitiesPage() {
   return (
     <>
       <h1 className='flex items-center text-2xl leading-[42px] font-bold'>예약 현황</h1>
-      <div className='mx-auto mt-[100px] flex w-full max-w-[1200px] flex-col items-center justify-center'>
-        <div className='relative h-[140px] w-[140px] md:h-[200px] md:w-[200px]'>
-          <Image src={Empty} fill alt='검색 결과 0개' className='absolute' />
-        </div>
-        <p className='text-2lg mt-[24px] font-medium text-gray-800'>아직 등록한 체험이 없어요.</p>
-      </div>
+      <Empty>아직 등록한 체험이 없어요</Empty>
     </>
   );
 }
