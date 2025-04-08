6f528d11f79e6cd9e0532b2286e715e58e41958f
nagyum <wildanimal19@naver.com>
2025-04-08 11:33:07 +0900

    fix:파일명 수정

--- src/app/(with-nav-footer)/(mypage)/my-activities/[id]/edit-activities/page.tsx
+++ src/app/(with-nav-footer)/(mypage)/my-activities/[id]/edit-activities/page.tsx
@@ -1,4 +1,4 @@
-import EditActivityPage from './_components/EditActivity';
+import EditActivityPage from './_components/EditActivities';
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const resolvedParams = await params;
