d49737334d881b28047c9b73750623b465a21eaa
qpalkim <ghs0199@naver.com>
2025-04-09 12:58:06 +0900

    refactor: 배너 클릭 시 해당 상세 페이지 이동

--- src/app/(with-nav-footer)/activities/_components/BestActivitiesBanner.tsx
+++ src/app/(with-nav-footer)/activities/_components/BestActivitiesBanner.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import BestActivitiesBannerSkeleton from './BestActivitiesBannerSkeleton';
 import RetryError from '@/components/RetryError';
 import { useActivities } from '@/lib/hooks/useActivities';
@@ -42,28 +43,30 @@ export default function BestActivitiesBanner() {
 
           return (
             <SwiperSlide key={activity.id}>
-              <div className='relative h-[550px] w-full'>
-                <Image
-                  src={activity.bannerImageUrl}
-                  alt={`${activity.title} 배너 이미지`}
-                  fill
-                  priority={index === 0}
-                  sizes='100vw'
-                  quality={100}
-                  className='object-cover'
-                />
-              </div>
-              <div className='absolute inset-0 bg-gradient-to-r from-black/70 to-transparent' />
-              <div className='absolute inset-0 z-10 mx-auto my-[70px] max-w-[1200px] px-6 text-white md:my-[160px]'>
-                <h2 className='text-2xl font-bold text-white md:text-[54px] md:leading-[60px]'>
-                  {firstLine}
-                  <br />
-                  {secondLine}
-                </h2>
-                <p className='text-md my-[8px] font-semibold text-white md:my-[20px] md:text-xl'>
-                  이달의 인기 체험 BEST 🔥
-                </p>
-              </div>
+              <Link href={`/activities/${activity.id}`}>
+                <div className='relative h-[550px] w-full'>
+                  <Image
+                    src={activity.bannerImageUrl}
+                    alt={`${activity.title} 배너 이미지`}
+                    fill
+                    priority={index === 0}
+                    sizes='100vw'
+                    quality={100}
+                    className='object-cover'
+                  />
+                </div>
+                <div className='absolute inset-0 bg-gradient-to-r from-black/70 to-transparent' />
+                <div className='absolute inset-0 z-10 mx-auto my-[70px] max-w-[1200px] px-6 text-white md:my-[160px]'>
+                  <h2 className='text-2xl font-bold text-white md:text-[54px] md:leading-[60px]'>
+                    {firstLine}
+                    <br />
+                    {secondLine}
+                  </h2>
+                  <p className='text-md my-[8px] font-semibold text-white md:my-[20px] md:text-xl'>
+                    이달의 인기 체험 BEST 🔥
+                  </p>
+                </div>
+              </Link>
             </SwiperSlide>
           );
         })}
